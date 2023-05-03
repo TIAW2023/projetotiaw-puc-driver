dd8b9bb443d2fe0c9f6015a234112c69867bb710
Gustavo Oliveira <gusoliv3ira@gmail.com>
2023-05-03 09:52:08 -0300

    Tela perfil do motorista e feedback (#1)
    * checkpoint na tela de feedback do motorista
    
    * finish to implement 
    
    * adicionando perfil de feedback e perfil de motorista

---
+++ codigo/js/perfil-motorista.js
@@ -0,0 +1,48 @@
+const tabs = document.querySelectorAll('[data-tab-value]');
+const tabInfos = document.querySelectorAll('[data-tab-info]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabValue);
+
+        tabInfos.forEach(tabInfo => {
+            tabInfo.classList.remove('active');
+        });
+
+        target.classList.add('active');
+    })
+});
+
+/*
+$('.nav-link').get().forEach(tab => {
+    tab.addEventListener('click', (tabClicked) => {
+        console.log($(this).attr("id"));
+    })
+});
+*/
+
+$('a.nav-link').click(function(){
+    //add active class to item clicked and remove other ones
+    $(this).addClass('active').parent().siblings().children().removeClass('active');
+
+    switch(this.id){
+        case 'tab-1': 
+            $('#tab_1').load('../../html/motorista/dados-basicos.html')
+            break;
+        case 'tab-2': 
+            $('#tab_2').load('../../html/motorista/info-van.html')
+            break;
+        case 'tab-3': 
+            $('#tab_3').load('../../html/motorista/anexos.html')
+            break;
+        case 'tab-4': 
+            $('#tab_4').load('../../html/motorista/disponibilidade.html')
+            break;
+        default:
+            break;
+    }
+});
+
+$(document).ready(function() {
+    $('#tab_1').load('../../html/motorista/dados-basicos.html');
+});
