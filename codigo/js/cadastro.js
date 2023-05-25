2d6a0e095b32ca3c1d0b7c612c5592bfc28ba0b8
Victor Távora Valadares Coelho <452735@sga.pucminas.br>
2023-05-25 18:45:45 -0300

    banco

--- codigo/js/cadastro.js
+++ codigo/js/cadastro.js
@@ -1,4 +1,9 @@
+var cadastroJSON = JSON.parse(localStorage.getItem("motoristas"));
+if (!cadastroJSON){
+  cadastroJSON=[]
+}
 function salvarCadastro() {
+
   var nome = document.getElementById("nome").value;
   var cpf = document.getElementById("cpf").value;
   var telefone = document.getElementById("telefone").value;
@@ -8,10 +13,36 @@ function salvarCadastro() {
     nome: nome,
     cpf: cpf,
     telefone: telefone,
-    email: email
+    email: email,
+    senha: senha,
+    placa: "",
+    chassi: "",
+    ano: "",
+    crlv: "",
+    van: "",
+    selfie: "",
+    turno: "",
+    bairros: ""
   };
 
-  var cadastroJSON = JSON.stringify(cadastro);
+  cadastroJSON.push(cadastro);
 
-  localStorage.setItem("cadastro", cadastroJSON);
+  var id=parseInt(cadastroJSON.lenght-1);
+  alert(id);
+
+
+/*
+  localStorage.setItem("motoristas", cadastroJSON);
+  
+  var cadastroJSON = JSON.stringify(cadastro);
+  var verificar = localStorage.getItem("cadastro");
+  if (verificar.length==0){
+  localStorage.setItem("motoristas", cadastroJSON);}
+  else {
+    verificar JSON.stringify(verificar).push}
+    */
+}
+function salvarParte2(){
+
+  alert(cadastroJSON);
 }
